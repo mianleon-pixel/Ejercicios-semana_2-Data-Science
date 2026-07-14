30391d647759b4b16fa386470017061570a5fefb
mianleon-pixel <mianleon@gmail.com>
2026-07-13 23:17:32 -0500

    Eliminado archivo extra desde el explorador de Windows

--- mi_primer_proyecto.py
+++
@@ -1,7 +0,0 @@
-numeros = [4, 7, 2, 4, 9, 7, 1, 2, 10, 4]
-# 2. Creamos una lista vacía para guardar los números únicos
-unicos = []
-for n in numeros:
-    if n not in unicos:
-        unicos.append(n)
-print("lista sin duplicados", unicos)
